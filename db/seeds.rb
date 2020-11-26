54e23defa0f3559ed4e6b05c983258b8a8b02868
J-sunfish <oga09kazu16@gmail.com>
2020-11-26 18:08:09 +0900

    seedファイルの記述削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-require 'csv'
-users_csv = CSV.readlines("db/users.csv")
-users_csv.shift
-users_csv.each do |row|
-  user = User.new
-  user.email = row[1]
-  user.password = row[2]
-  user.name = row[3]
-  user.profile = row[4]
-  user.occupation = row[5]
-  user.position = row[6]
-  puts user.save!
-end
